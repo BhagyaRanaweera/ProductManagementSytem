546c4006c4e07a25719184f6c1c0247ffe5dbf6c
sara2025-gif <aajhandunneththi@gmail.com>
2024-12-03 00:05:08 +0530

    Add orderItem files

--- src/main/java/com/example/productorderingsystem/service/interf/OrderItemService.java
+++ src/main/java/com/example/productorderingsystem/service/interf/OrderItemService.java
@@ -1,5 +1,14 @@
 package com.example.productorderingsystem.service.interf;
+import com.example.productorderingsystem.dto.OrderRequest;
+import com.example.productorderingsystem.dto.Response;
+import com.example.productorderingsystem.enums.OrderStatus;
+import org.springframework.data.domain.Pageable;
 
-public class OrderItemService {
+import java.time.LocalDateTime;
 
+public interface OrderItemService {
+    Response placeOrder(OrderRequest orderRequest);
+    Response updateOrderItemStatus(Long orderItemId, String status);
+    Response filterOrderItems(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate, Long itemId, Pageable pageable);
 }
+
