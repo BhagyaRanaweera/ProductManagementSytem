10b467200240dca4d71fd570c4d5fc9df05cda25
AmaThathsarani99 <amalithathsarani99@gmail.com>
2024-12-02 23:51:34 +0530

    add new files

--- src/main/java/com/example/productorderingsystem/service/interf/CategoryService.java
+++ src/main/java/com/example/productorderingsystem/service/interf/CategoryService.java
@@ -1,5 +1,13 @@
 package com.example.productorderingsystem.service.interf;
 
-public class CategoryService {
+import com.example.productorderingsystem.dto.CategoryDto;
+import com.example.productorderingsystem.dto.Response;
 
+public interface CategoryService {
+
+    Response createCategory(CategoryDto categoryRequest);
+    Response updateCategory(Long categoryId, CategoryDto categoryRequest);
+    Response getAllCategories();
+    Response getCategoryById(Long categoryId);
+    Response deleteCategory(Long categoryId);
 }
