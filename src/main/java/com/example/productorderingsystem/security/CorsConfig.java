edfb8d2adc4c5bda1b28ca0f939b504cea0a8def
BhagyaRanaweera <sithubhagya01@gmail.com>
2025-01-05 10:38:33 -0500

    add corsconfig changes

--- src/main/java/com/example/productorderingsystem/security/CorsConfig.java
+++ src/main/java/com/example/productorderingsystem/security/CorsConfig.java
@@ -1,9 +1,10 @@
 package com.example.productorderingsystem.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @Configuration
 public class CorsConfig {
 
@@ -11,8 +12,7 @@ public class CorsConfig {
     public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings( CorsRegistry registry){
-           
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins("*");
