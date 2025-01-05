bc9635fd6a3d00967fbec8a3f0bf08ff4446a7cf
BhagyaRanaweera <sithubhagya01@gmail.com>
2025-01-05 10:38:17 -0500

    add productrepo changes

--- src/main/java/com/example/productorderingsystem/repository/ProductRepo.java
+++ src/main/java/com/example/productorderingsystem/repository/ProductRepo.java
@@ -4,8 +4,12 @@ package com.example.productorderingsystem.repository;
 import com.example.productorderingsystem.entity.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+//import java.util.Optional;
 
 public interface ProductRepo extends MongoRepository<Product,String> {
-    List<Product> findByCategoryId(Long categoryId);
+    List<Product> findByCategoryId(String categoryId);
     List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+
+
+    
 }
