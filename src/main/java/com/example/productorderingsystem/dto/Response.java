f096b5525aa3c8131ea8e0161ca847f94395739f
BhagyaRanaweera <sithubhagya01@gmail.com>
2025-01-05 10:21:23 -0500

    add response entity changes

--- src/main/java/com/example/productorderingsystem/dto/Response.java
+++ src/main/java/com/example/productorderingsystem/dto/Response.java
@@ -1,14 +1,18 @@
 package com.example.productorderingsystem.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+
+
 @Data
 @Builder
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
 
