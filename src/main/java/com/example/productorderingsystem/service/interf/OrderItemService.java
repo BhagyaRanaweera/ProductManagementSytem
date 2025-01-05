58c063c364a0f99708f1232c63e72795aed8217c
BhagyaRanaweera <sithubhagya01@gmail.com>
2025-01-05 11:37:56 -0500

    add orderitemservice changes

--- src/main/java/com/example/productorderingsystem/service/interf/OrderItemService.java
+++ src/main/java/com/example/productorderingsystem/service/interf/OrderItemService.java
@@ -1,4 +1,8 @@
 package com.example.productorderingsystem.service.interf;
+
+
+
+
 import com.example.productorderingsystem.dto.OrderRequest;
 import com.example.productorderingsystem.dto.Response;
 import com.example.productorderingsystem.enums.OrderStatus;
@@ -8,7 +12,6 @@ import java.time.LocalDateTime;
 
 public interface OrderItemService {
     Response placeOrder(OrderRequest orderRequest);
-    Response updateOrderItemStatus(Long orderItemId, String status);
-    Response filterOrderItems(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate, Long itemId, Pageable pageable);
+    Response updateOrderItemStatus(String orderItemId, String status);
+    Response filterOrderItems(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate, String itemId, Pageable pageable);
 }
-
