309e7eb442106a742268a50bf56a3863604c49df
BhagyaRanaweera <sithubhagya01@gmail.com>
2025-03-04 01:02:44 +0530

    add changes cors

--- src/main/java/com/example/productorderingsystem/security/CorsConfig.java
+++ src/main/java/com/example/productorderingsystem/security/CorsConfig.java
@@ -7,16 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
+    public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Allow your frontend URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Include OPTIONS for preflight requests
-                        .allowCredentials(true); // Allow credentials if needed
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
             }
         };
     }
