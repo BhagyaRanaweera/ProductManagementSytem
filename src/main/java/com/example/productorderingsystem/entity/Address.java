e6843be0c9df3fdaf923d7ad18676aa55bc2f130
BhagyaRanaweera <sithubhagya01@gmail.com>
2025-03-04 00:59:32 +0530

    add address entity

--- src/main/java/com/example/productorderingsystem/entity/Address.java
+++ src/main/java/com/example/productorderingsystem/entity/Address.java
@@ -1,29 +1,28 @@
 package com.example.productorderingsystem.entity;
 
-
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+
+
 @Data
-@Document(collection = "addresses")
+@Document(collection = "addresses") // MongoDB collection name
 public class Address {
 
     @Id
-    private String id;
-
+    private String id;  // MongoDB will automatically use this as the document ID
+    
     private String street;
     private String city;
     private String state;
     private String zipCode;
     private String country;
-
-    // Use @DBRef to reference the User document
-    @DBRef
+    
     private User user;
-
-    private final LocalDateTime createdAt = LocalDateTime.now();
+    
+    private LocalDateTime createdAt;
 }
